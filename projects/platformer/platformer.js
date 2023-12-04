212335e371e18934f76187ed333ddb261f37653c
miya rolston <miyarolston1@gmail.com>
2023-12-04 15:58:44 +0000

    added platform

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,7 +43,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(500, 600, 200, 20)
+    createPlatform(500, 620, 200, 20)
+    createPlatform(600, 580, 200, 20)
 
 
 
